cd1e24c58f71a053493264c4467b8a157d0dc2c8
Camilo Rodriguez <camilorodriguezco7@gmail.com>
2023-07-12 17:55:40 -0500

    Pagina de agregar colaborador lista, buscando colaboradores y mostrando en pantalla

--- src/context/ProyectosProvider.jsx
+++ src/context/ProyectosProvider.jsx
@@ -13,6 +13,7 @@ const ProyectosProvider = ({children}) => {
     const [ modalFormularioTarea, setModalFormularioTarea] = useState(false);
     const [ tarea, setTarea ] = useState({});
     const [ modalEliminarTarea, setModalEliminarTarea ] = useState(false);
+    const [ colaborador, setColaborador ] = useState({})
 
     const navigate = useNavigate();
 
@@ -185,7 +186,7 @@ const ProyectosProvider = ({children}) => {
         }
 
     }
-    
+
     const editarTarea = async tarea =>{
         try {
             const token = localStorage.getItem('token')
@@ -280,6 +281,31 @@ const ProyectosProvider = ({children}) => {
         }
     }
 
+    const submitColaborador = async email => {
+        setCargando(true)
+        try {
+            const token = localStorage.getItem('token')
+            if(!token) return
+
+            const config = {
+                headers: {
+                    "Content-Type" : "application/json",
+                    Authorization: `Bearer ${token}`
+                }
+            }
+            const { data } = await clienteAxios.post('/proyectos/colaboradores', {email}, config)
+            setColaborador(data)
+            setAlerta({})
+        } catch (error) {
+            setAlerta({
+                msg: error.response.data.msg,
+                error: true
+            })
+        } finally {
+            setCargando(false)
+        }
+    }
+
     return (
         <ProyectosContext.Provider
             value={{
@@ -298,7 +324,10 @@ const ProyectosProvider = ({children}) => {
                 tarea,
                 modalEliminarTarea,
                 handleModalEliminarTarea,
-                eliminarTarea
+                eliminarTarea,
+                submitColaborador,
+                colaborador,
+                setColaborador
             }}
         >
         {children}
--- src/paginas/Proyecto.jsx
+++ src/paginas/Proyecto.jsx
@@ -70,6 +70,14 @@ const Proyecto = () => {
 
           </div>
 
+          <div className="flex items-center justify-between mt-10">
+            <p className="font-bold text-xl mt-10">Colaboradores</p>
+            <Link
+              to={`/proyectos/nuevo-colaborador/${proyecto._id}`}
+              className="text-gray-400 hover:text-black uppercase font-bold"
+            >Añadir</Link>
+          </div>
+
           <ModalFormularioTarea />
           <ModalEliminarTarea />
     </>
